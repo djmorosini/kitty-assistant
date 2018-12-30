9528e956b498a2a6b620d6159c3a3f2c57bdbf26
djmorosini <djmorosini@outlook.com>
2018-12-30 00:26:52 -0500

    Updates

---
+++ precache-manifest.fef1a91028117a5ffd4c13b9866f1b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0844a8a281698ac5c012e39fecf3377f",
+    "url": "/kitty-assistant/static/media/Yarn_Kitty.0844a8a2.png"
+  },
+  {
+    "revision": "5d43d2edf868995b9a4a",
+    "url": "/kitty-assistant/static/js/runtime~main.5d43d2ed.js"
+  },
+  {
+    "revision": "9f3710b2ec45d386326e",
+    "url": "/kitty-assistant/static/js/main.9f3710b2.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/kitty-assistant/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "9f3710b2ec45d386326e",
+    "url": "/kitty-assistant/static/css/main.0b10288b.chunk.css"
+  },
+  {
+    "revision": "bcc05d6c892f02ab8a4a76c33f145f02",
+    "url": "/kitty-assistant/index.html"
+  }
+];
