1ee2c13a60008d6640048908dba8ab5054859868
djmorosini <djmorosini@outlook.com>
2018-12-30 00:20:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TypingGame from './TypingGame'
+import YarnKitty from './sounds/Yarn_Kitty.png'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentGame: null
+    }
+  }
+
+  updateGame = (game) => {
+    if (game === 'typing') {
+      this.setState({ currentGame: game })
+    } else {
+      this.setState({ currentGame: null })
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    let currentGame = this.state.currentGame
+    if (currentGame === null) {
+      return (
+        <div className="App">
+          <h1>Kitty Assistant</h1>
+          <img src={YarnKitty} alt='kitty-assistant' />
+          <h2>Choose Game</h2>
+          <button onClick={() => this.updateGame('typing')}>Typing</button>
+        </div>
+      );
+    } else if (currentGame === 'typing') {
+      return (
+        <div id='typing-game'>
+          <TypingGame updateGame={this.updateGame} />
+        </div>
+      )
+    }
   }
 }
 
